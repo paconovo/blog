4098296b2e9ec1133c6098fe798a44f008b8d813
Paco <elpaquinho@gmail.com>
2023-06-04 17:59:23 +0200

    seeders de usuarios

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'full_name' => 'Pepe',
+            'email' => 'pepe@pepe.com',
+            'password' => Hash::make('123456')
+        ]);
+
+        User::create([
+            'full_name' => 'manuel',
+            'email' => 'manuel@pepe.com',
+            'password' => Hash::make('1234546')
+        ]);
+        User::factory(10)->create();
+    }
+}
